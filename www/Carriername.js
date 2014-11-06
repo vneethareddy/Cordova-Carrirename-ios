3a9672b80533a9e7a40660115113fae88e7b97ce
vneethareddy <vneethareddy@gmail.com>
2014-11-06 15:08:04 +0530

    Update Carriername.js

--- www/Carriername.js
+++ www/Carriername.js
@@ -1,24 +1,47 @@
-// cordova.define("Carriername.Carriername", function(require, exports, module) {   
+cordova.define("CarrierName.CarrierName", function(require, exports, module) {
                
-               var exec = require('cordova/exec');
-
-         
-              
+               var argscheck = require('cordova/argscheck'),
+               channel = require('cordova/channel'),
+               utils = require('cordova/utils'),
+               exec = require('cordova/exec'),
+               cordova = require('cordova');
                
-//               Carriername.prototype.getCarrierInfo = function(successCallback, errorCallback) {
-//                exec(successCallback, errorCallback, "Carriername", "getCarrierName", []);
-//               };
-//               };
+               channel.createSticky('onCordovaInfoReady');
+               // Tell cordova channel to wait on the CordovaInfoReady event
+               channel.waitForInitialization('onCordovaInfoReady');
+   
                
+               function CarrierName() {
+               this.available = false;
+               this.carriername = null;
+               this.mcc = null;
+               this.mnc = null;
                
-               var Carriername = {
-               getCarrierName:function() {
-               exec(null, null, "Carriername", "getCarrierName", []);
+               var me = this;
+               
+               channel.onCordovaReady.subscribe(function() {
+                                                me.getInfo(function(info) {
+                                                           //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
+                                                           //TODO: CB-5105 native implementations should not return info.cordova
+                                                           var buildLabel = cordova.version;
+                                                           me.available = true;
+                                                           me.carriername = info.carriername;
+                                                           me.mcc = info.mcc;
+                                                           me.mnc = info.mnc;
+                                                             channel.onCordovaInfoReady.fire();
+                                                           },function(e) {
+                                                           me.available = false;
+                                                           utils.alert("[ERROR] Error initializing Cordova: " + e);
+                                                           });
+                                                });
                }
                
+
+               CarrierName.prototype.getInfo = function(successCallback, errorCallback) {
+               argscheck.checkArgs('fF', 'CarrierName.getInfo', arguments);
+               exec(successCallback, errorCallback, "CarrierName", "getCarrierName", []);
                };
                
+               module.exports = new CarrierName();
                
-              module.exports = new Carriername();
-
-});
+               });
